6d8df27704e46f9105073ed1093c259ffe770fd3
Nathania Devina Godjali <devinagodjali@gmail.com>
2017-07-22 16:31:49 +0700

    Second commit, add data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { Div1Component } from './div1/div1.component';
+import { Div2Component } from './div2/div2.component';
+import { Div3Component } from './div3/div3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    Div1Component,
+    Div2Component,
+    Div3Component
   ],
   imports: [
     BrowserModule
